05b4a8aa150620906643ffbfe89e4dd99592a65c
peterthemiller <peter.the.miller@gmail.com>
2021-11-03 12:37:43 -0500

    Finalized topology planning, made vlans per-CN named, numbered, and described. Additionally added preliminary templates for access and trunk ports

--- generate.py
+++ generate.py
@@ -11,6 +11,21 @@ output_directory = "_output"
 if not os.path.exists(output_directory):
     os.mkdir(output_directory)
 
+
+def customize_vlans(core_node, vlan_list):
+    '''
+    Takes in a Core Node, string like "CN1" and a list of vlan dictionaries
+    renames vlans, and gives them a vlan-id per core node. 
+    for now, only takes in single-digit CN numbers
+    returns a list of vlan dictionaries
+    '''
+    for vlan in vlan_list:
+        vlan['name'] = core_node + '_' + vlan['name']
+        vlan['description'] = core_node + '_' + vlan['description']
+        vlan['id'] = core_node[-1] + str(vlan['id'])
+    return vlan_list
+
+
 # press c to continue
 # type a variable name to see the data
 # pdb.set_trace()
@@ -21,18 +36,23 @@ loader = FileSystemLoader('./templates')
 # https://blog.networktocode.com/post/whitespace-control-in-jinja-templates/ on whitespace control
 # or look https://ttl255.com/jinja2-tutorial-part-3-whitespace-control/ at this if you want to die
 env = Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
-
 vlan_temp = env.get_template('vlans.j2')
 rendered = vlan_temp.render(vlans)
 
 
 for device in devices['devices']:
+    # adding in vlan template to device template
+    device['vlans'] = vlans['vlans']
+    # transforming device vlan configuration to be CN-specific
+    device['vlans'] = customize_vlans(device['CN'], device['vlans'])
+    # loading in device template
     device_template = env.get_template('device_template.j2')
+    # render device .conf file
     device_config = device_template.render(device)
+    # creates file in ./_output
     f = open(os.path.join(output_directory,
              device['hostname'] + ".conf"), "w+")
-    f.write(device_config + '\n')
-    f.write(rendered)
+    f.write(device_config)
     f.close()
     print(f"Configuration for {device['hostname']} created")
 print("DONE")
