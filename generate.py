5e6f7a545ef3bd3eaae7c7c3bb03adaae550686b
peterthemiller <peter.the.miller@gmail.com>
2021-11-04 11:34:17 -0500

    - created IRB template to apply to hostnames with "DN" - updated naming scheme for devices, according to lab topology - fixed some semicolon placement issues - simplified main script by implement VLAN renaming in jinja instead of for loop - trunk ports and access ports templates implemented into interfaces.j2

--- generate.py
+++ generate.py
@@ -2,6 +2,8 @@ from jinja2 import Environment, FileSystemLoader
 import yaml
 import pdb
 import os
+import copy
+
 with open('devices.yml', 'r') as f:
     devices = yaml.safe_load(f)
 
@@ -10,20 +12,6 @@ with open('vlans.yml', 'r') as f:
 output_directory = "_output"
 if not os.path.exists(output_directory):
     os.mkdir(output_directory)
-
-
-def customize_vlans(core_node, vlan_list):
-    '''
-    Takes in a Core Node, string like "CN1" and a list of vlan dictionaries
-    renames vlans, and gives them a vlan-id per core node. 
-    for now, only takes in single-digit CN numbers
-    returns a list of vlan dictionaries
-    '''
-    for vlan in vlan_list:
-        vlan['name'] = core_node + '_' + vlan['name']
-        vlan['description'] = core_node + '_' + vlan['description']
-        vlan['id'] = core_node[-1] + str(vlan['id'])
-    return vlan_list
 
 
 # press c to continue
@@ -36,15 +24,9 @@ loader = FileSystemLoader('./templates')
 # https://blog.networktocode.com/post/whitespace-control-in-jinja-templates/ on whitespace control
 # or look https://ttl255.com/jinja2-tutorial-part-3-whitespace-control/ at this if you want to die
 env = Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
-vlan_temp = env.get_template('vlans.j2')
-rendered = vlan_temp.render(vlans)
-
 
 for device in devices['devices']:
-    # adding in vlan template to device template
     device['vlans'] = vlans['vlans']
-    # transforming device vlan configuration to be CN-specific
-    device['vlans'] = customize_vlans(device['CN'], device['vlans'])
     # loading in device template
     device_template = env.get_template('device_template.j2')
     # render device .conf file
