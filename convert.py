ac96e796d9b521dda610f99de9ba7a62286e4d75
peterthemiller <peter.the.miller@gmail.com>
2021-11-22 15:52:15 -0600

    renamed set files to be .txt, fixed bracketing on irb interfaces.

--- convert.py
+++ convert.py
@@ -1,11 +1,12 @@
 import juniper_display_set.junos_converter
 import os
 import sys
-results_folder = [file for file in os.listdir('_output') if os.path.isfile]
 if not os.path.exists('_output/set'):
     set_folder = os.mkdir('_output/set')
+results_folder = [file for file in os.listdir('_output') if os.path.isfile]
+results_folder.remove('set')
 for device in results_folder:
-    with open('_output/set/' + device, 'w+') as f:
+    with open('_output/set/' + device[:-4] + 'set.txt', 'w+') as f:
         sys.stdout = f
         juniper_display_set.junos_converter.get_set_config(
             ('_output/' + device), False)
